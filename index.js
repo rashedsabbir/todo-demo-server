ad98936e9f473adffd6899f7a16f96a88aca92af
rashedsabbir <rashedulhasan1996@gmail.com>
2022-07-01 20:52:20 +0600

    monogodb connected

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId=require("mongodb").ObjectId
 const port = process.env.PORT || 5000;
+require("dotenv").config()
 const cors = require("cors");
 
+app.get("/", (req, res) => {
+    res.send("Server running successfully");
+  });
 
+  app.get("/check",(req,res)=>{
+    res.send("Checking  this for remote url change")
+})
+  
 app.use(cors({
     origin: "*"
   }));
   app.use(express.json());
   
   
+// Mongo connect 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.poglh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
   async function run() {
     try {
+
+        await client.connect();
+        const tasksCollection = client.db("todo-manager").collection("task");
+        const completeTaskCollection = client.db("complete-todo").collection("complete");
+
+        
 
         console.log("Connected to MongoDB database");
   } finally {
@@ -23,9 +42,7 @@ app.use(cors({
 run().catch(console.dir);
 
 
-app.get("/", (req, res) => {
-  res.send("Server running successfully");
-});
+
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
